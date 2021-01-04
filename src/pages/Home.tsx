f1d27bf3b7e160cc4b48e9fdd6f04b39856eb79c
dariodasilva <dariotecinfo@gmail.com>
2021-01-04 19:27:19 -0300

    Mobile App em Ionic 5 - React - Capacitor

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,12 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, 
+         IonList, IonItem, IonCheckbox, IonLabel, IonNote, IonBadge, IonFab, IonFabButton, IonIcon } from '@ionic/react';
 import React from 'react';
 import ExploreContainer from '../components/ExploreContainer';
+import { add } from 'ionicons/icons';
 import './Home.css';
+import { RouteComponentProps } from 'react-router';
 
-const Home: React.FC = () => {
+const Home: React.FC<RouteComponentProps> = (props) => {
   return (
     <IonPage>
       <IonHeader>
@@ -11,14 +14,25 @@ const Home: React.FC = () => {
           <IonTitle>Blank</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
+      <IonContent>
+        <IonList>
+          <IonFab vertical="bottom" horizontal="end" slot="fixed">
+            <IonFabButton onClick={() => props.history.push('/new')}>
+              <IonIcon icon={add} />
+            </IonFabButton>
+          </IonFab>
+          <IonItem >
+            <IonCheckbox slot="start" />
+            <IonLabel>
+              <h1>Create Idea</h1>
+              <IonNote>Run Idea by Brandy</IonNote>
+            </IonLabel>
+            <IonBadge color="success" slot="end">
+              5 Days
+            </IonBadge>
+          </IonItem>
+        </IonList>
+  </IonContent>
     </IonPage>
   );
 };
